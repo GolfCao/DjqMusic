f5dfc713fb728ddeb70bc0c1a2ec08a798e73891
caoguofeng <634848121@qq.com>
2019-12-11 15:28:36 +0800

    [DjqMusic] init project

---
+++ app/src/main/java/com/caoguofeng/djqmusic/Consts.java
@@ -0,0 +1,7 @@
+package com.caoguofeng.djqmusic;
+
+public class Consts {
+    public static final String ENDPOINT = BuildConfig.ENDPOINT;
+    public static final String RESOURCE_PREFIX = BuildConfig.RESOURCE_PREFIX;
+
+}
